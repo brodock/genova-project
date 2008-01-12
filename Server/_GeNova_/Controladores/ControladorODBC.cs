a3494174558f911bd8e33cbc7ae118d3c18dd21e
Alex Marçal Souza <souzaemarcal@gmail.com>
2008-01-12 21:09:26 +0000

    git-svn-id: http://genovaproject.googlecode.com/svn/trunk@28 b0b2c0a1-3543-0410-befa-599e5dd28ab0

--- Server/_GeNova_/Controladores/ControladorODBC.cs
+++ Server/_GeNova_/Controladores/ControladorODBC.cs
@@ -1,3 +1,18 @@
+/***************************************************************************
+ *   copyright            : (C) GeNova Project
+ *   webSite              : http://code.google.com/p/genovaproject/
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
 using System;
 using System.Text;
 using System.Collections.Generic;
