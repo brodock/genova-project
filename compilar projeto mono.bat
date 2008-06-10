22a1384708caa222b6a06e16843fa370d8e93af1
Alex Marçal Souza <souzaemarcal@gmail.com>
2008-06-10 03:58:35 +0000

    Correction facing support Mono (Last Version).
    git-svn-id: http://genovaproject.googlecode.com/svn/trunk@60 b0b2c0a1-3543-0410-befa-599e5dd28ab0

--- compilar projeto mono.bat
+++ compilar projeto mono.bat
@@ -2,12 +2,12 @@
 REM COMPILAR NO WINDOWS COM MONO
 
 REM Change the directory below based on your machine.
-cd "C:\GeNova\GeNova 3.0"
+cd "E:\Projetos\GeNova\GeNova 3.0"
 IF EXIST Runuo_Mono.exe DEL Runuo_Mono.exe
 
 REM Change the directory below based on your machine.
-cd "C:\GeNova\GeNova 3.0\Server"
-set FRAMEWORK=C:\"Program Files"\Mono\bin
+cd "E:\Projetos\GeNova\GeNova 3.0\Server"
+set FRAMEWORK=E:\"Arquivos de programas"\Mono-1.9.1\bin
 %FRAMEWORK%\gmcs -define:MONO -unsafe+ -optimize -target:exe -out:..\RunUO_Mono.exe -win32icon:genova.ico -recurse:*.cs -reference:MySql.Data.dll -reference:System.Data.dll -reference:System.Drawing.dll -reference:Ultima.dll
 
 REM COMPILAR NO LINUX COM MONO
